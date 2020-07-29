05344396665cd44c7b141e805eabd60411cef3f8
Augustine Calvino <acalvino4@Augustines-MacBook-Pro.local>
2020-07-29 16:55:43 -0600

    hook up more interactivity

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,9 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -40,13 +43,25 @@ class App extends React.Component {
 
     this.setState({playlistTracks: tracks})
   }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+  
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
   
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
@@ -56,6 +71,8 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
