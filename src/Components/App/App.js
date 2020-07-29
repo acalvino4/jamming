bed6876a69312a28509125180e8e05b623b9a3e9
Augustine Calvino <acalvino4@Augustines-MacBook-Pro.local>
2020-07-29 16:02:46 -0600

    enable tracklist modifications

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,7 +20,25 @@ class App extends React.Component {
         {name: 'a', artist: 'a', album: 'a', id: '1'},
         {name: 'd', artist: 'd', album: 'd', id: '4'},
       ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
     }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks})
   }
   
   render() {
@@ -30,10 +48,14 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist 
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
